24b8c61c91b2495a5866d2a23064c40a3041bbc2
Dr. Wang <37176118+wangyong950511@users.noreply.github.com>
2025-04-24 09:08:41 +0800

    Update 服务器.sh

--- Shiny_server/服务器.sh
+++ Shiny_server/服务器.sh
@@ -2,7 +2,7 @@
 sudo cp qpcr_module.R protein_module.R primer_module.R app.R /srv/shiny-server/autorun/
 
 # 查看日志
-tail -f $(ls -t /var/log/shiny-server/myapp-*.log | head -n 1)
+tail -f $(ls -t /var/log/shiny-server/autorun-*.log | head -n 1)
 
 # 重启shiny-server
 sudo systemctl restart shiny-server
